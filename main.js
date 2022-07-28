f1affea9eb27629392b7f3fd0f9b24c7a6bab0c5
Julius Oh <juliusoh@gmail.com>
2022-07-28 00:45:34 -0700

    refactor

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 import shell_cmd from "./shell_cmd.js";
 import { ExportToCsv } from "export-to-csv";
-import fs from 'fs';
+import fs from "fs";
+import _ from "lodash";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 let projects = [];
 async function checkForProject() {
   try {
+    const cloudAssets = new shell_cmd();
+    await cloudAssets.execCommand(`gcloud services enable cloudasset.googleapis.com`);
     const start = new shell_cmd();
     const result = await start.execCommand(
-      "gcloud projects list --format=json"
+      `gcloud asset search-all-resources --asset-types="cloudresourcemanager.googleapis.com/Project" --scope=organizations/${process.env.ORG_ID} --format=json`
     );
     const data = JSON.parse(result);
     data.forEach((project) => {
-      projects.push(project.projectId);
+      projects.push(project.additionalAttributes.projectId);
     });
     console.log(projects);
   } catch (error) {
@@ -21,40 +27,32 @@ async function checkForProject() {
 }
 
 await checkForProject();
+
+let finalData = [];
 async function getComputeList() {
   try {
-    projects.forEach(async (project) => {
-      const computeApi = new shell_cmd();
-      await computeApi.execCommand(`gcloud services enable compute.googleapis.com --project ${project}`)
-      const start = new shell_cmd();
-      const result = await start.execCommand(
-        `gcloud compute instances list --project ${project} --format=json`
-      );
-      const data = JSON.parse(result);
-      const finalResult = data.map((instance) => ({
-        project: project,
-        name: instance.name,
-        machineType: instance.machineType.split("/").pop(),
-        status: instance.status,
-        zone: instance.zone.split("/").pop(),
-      }));
-      const options = {
-        fieldSeparator: ",",
-        quoteStrings: '"',
-        decimalSeparator: ".",
-        showLabels: true,
-        showTitle: true,
-        title: "List of Compute Instances",
-        useTextFile: false,
-        useBom: true,
-        useKeysAsHeaders: true,
-      };
-      const csvExporter = new ExportToCsv(options);
-      if (finalResult) {
-        const csvData = csvExporter.generateCsv(finalResult, true);
-        fs.writeFileSync('data.csv',csvData)
-      }
-    });
+    return Promise.all(
+      projects.map(async (project) => {
+        const computeApi = new shell_cmd();
+        await computeApi.execCommand(
+          `gcloud services enable compute.googleapis.com --project ${project}`
+        );
+        const start = new shell_cmd();
+        const result = await start.execCommand(
+          `gcloud compute instances list --project ${project} --format=json`
+        );
+        const data = JSON.parse(result);
+        data.forEach((instance) =>
+          finalData.push({
+            project: project,
+            name: instance.name,
+            machineType: instance.machineType.split("/").pop(),
+            status: instance.status,
+            zone: instance.zone.split("/").pop(),
+          })
+        );
+      })
+    );
   } catch (error) {
     console.log("**Error querying for instances >>>", error);
     return "Error looking for instance";
@@ -62,3 +60,19 @@ async function getComputeList() {
 }
 
 await getComputeList();
+
+const options = {
+  fieldSeparator: ",",
+  quoteStrings: '"',
+  decimalSeparator: ".",
+  showLabels: true,
+  showTitle: true,
+  title: "List of Compute Instances",
+  useTextFile: false,
+  useBom: true,
+  useKeysAsHeaders: true,
+};
+const csvExporter = new ExportToCsv(options);
+
+const csvData = csvExporter.generateCsv(finalData, true);
+fs.writeFileSync("data.csv", csvData);
